de14a661bb0c08c9e5aa5c0d6761e815099c63bb
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-19 23:32:36 +0530

    router added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
-function App() {
-  return <h1>Hello World!</h1>;
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const Course = () => (
+  <div>
+    <h2> Course </h2>
+  </div>
+);
+
+const Edpresso = () => (
+  <div>
+    <h2>Edpresso</h2>
+  </div>
+);
+
+export default function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/course">Course</Link>
+          </li>
+          <li>
+            <Link to="/edpresso">Edpresso</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Course />} />
+        <Route exact path="/edpresso" element={<Edpresso />} />
+      </Routes>
+    </>
+  );
 }
-
-export default App;
