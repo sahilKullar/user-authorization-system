2ed0d1eeb865da71ae4f0cb82b509af7710afd8b
Sahil Kullar <sahil_kullar@outlook.com>
2022-11-21 18:40:28 +0530

    built navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,7 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-
-const Home = () => (
-  <div>
-    <h2> Home </h2>
-  </div>
-);
-
-const Course = () => (
-  <div>
-    <h2> Course </h2>
-  </div>
-);
-
-const Edpresso = () => (
-  <div>
-    <h2>Edpresso</h2>
-  </div>
-);
+import Navbar from "./components/Navbar";
 
 export default function App() {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/course">Course</Link>
-          </li>
-          <li>
-            <Link to="/edpresso">Edpresso</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/course" element={<Course />} />
-        <Route exact path="/edpresso" element={<Edpresso />} />
-      </Routes>
-    </>
-  );
+  return <Navbar auth={false} />;
 }
